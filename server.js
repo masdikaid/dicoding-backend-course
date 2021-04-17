a8149009ef2d17d61272e5a01899fde18047c825
masdika ID <masdikaid@gmail.com>
2021-04-17 10:53:30 +0700

    initial file

---
+++ server.js
@@ -0,0 +1,19 @@
+import http from 'http'
+
+const requestListener = (request, response) => {
+    response.setHeader('Content-Type', 'text/html');
+    console.log(request)
+ 
+    response.statusCode = 200;
+    response.end('<h1>Halo HTTP Server!</h1>');
+};
+ 
+ 
+const server = http.createServer(requestListener);
+ 
+const port = 5000;
+const host = 'localhost';
+ 
+server.listen(port, host, () => {
+    console.log(`Server berjalan pada http://${host}:${port}`);
+});
